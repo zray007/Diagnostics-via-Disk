da6983c8b18ef8d33fc60d4a40e11e43799781c7
Stéphane Gourichon <stephane.gourichon@fidergo.fr>
2020-07-14 19:41:26 +0200

    Refine installation script with better dependencies, system-site packages.

--- install-debian-virtualenv.sh
+++ install-debian-virtualenv.sh
@@ -5,9 +5,10 @@ sudo apt-get install -y \
      python3 python3-pip python3-virtualenv \
      python3-qtpy \
      wodim \
-     libcdio-dev libiso9660-dev python3-all-dev
+     libcdio-dev libiso9660-dev pkg-config python3-all-dev swig
 
-virtualenv env/diagvdisk
+python3 -m virtualenv --system-site-packages -p /usr/bin/python3 env/diagvdisk
+
 source env/diagvdisk/bin/activate
 
 pip3 install qtpy guietta pycdio
